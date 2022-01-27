b6b5fb568ec6f4323ffa49397d8896ce3427eb8c
vivian-wis <63586518+vivian-wis@users.noreply.github.com>
2022-01-27 07:47:44 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -17,5 +17,5 @@ class Creds():
     
     #Example 
     #TG_TOKEN = "dkjfksdkffdkfdkfdj"
-    #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
-    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
+    #TEAMDRIVE_FOLDER_ID = "1t9nlizABMh7nnOp-p8NhPRl2n7KUZlse"
+    #TEAMDRIVE_ID = "0AIbLl7CfhBZ2Uk9PVA"
